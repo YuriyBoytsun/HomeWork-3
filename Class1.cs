af9568805c49e6a0487f061d8e90f92f70707bda
Yura Boytsun <sicksneg@gmail.com>
2021-12-24 15:52:06 +0100

    1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры. б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса. в) Добавить диалог с использованием switch демонстрирующий работу класса.

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace HW3._1
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex(int re, int im)
+        {
+            this.a = re;
+            this.b = im;
+        }
+       
+        
+
+        public string Print()
+        {
+            string s;
+            if (b < 0) s = $"{a} - {-b}i";
+            else s = $"{a} + {b}i";
+            return s;
+        }
+
+       public Complex Sum(Complex z)
+        {
+            Complex result = new(this.a + z.a, this.b + z.b);
+
+            return result;
+        }
+
+        public Complex Sub(Complex z)
+        {
+            Complex result = new(this.a - z.a, this.b - z.b);
+
+            return result;
+        }
+
+        public Complex Mul(Complex z)
+        {
+            Complex result = new((this.a*z.a) - (this.b*z.b),(this.b*z.a)+(this.a*z.b));
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HW3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
+
+");
+           
+
+            Console.WriteLine("Введите вещественную часть первого комплексного числа");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите мнимую часть первого комплексного числа");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите вещественную часть первого комплексного числа");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите мнимую часть первого комплексного числа");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+
+            Complex num1 = new Complex(a1,b1);
+            Complex num2 = new Complex(a2,b2);
+            Complex num3;
+
+            Console.WriteLine(num1.Print());
+            Console.WriteLine(num2.Print());
+
+            
+            Console.WriteLine("Сумма чисел нажмите 1.\nВычитание чисел нажмите 2.\nУмножение чисел нажмите 3");
+            int i = Convert.ToInt32(Console.ReadLine());
+            string s="";
+            switch (i)
+            {
+                case 1: num3 = num1.Sum(num2); s = $"РЕЗУЛЬТАТ СУММЫ:\n {num3.Print()}";
+                    break;
+                case 2: num3 = num1.Sub(num2); s = $"РЕЗУЛЬТАТ ВЫЧИТАНИЯ:\n {num3.Print()}";
+                    break;
+                case 3: num3 = num1.Mul(num2); s = $"РЕЗУЛЬТАТ УМНОЖЕНИЯ:\n {num3.Print()}";
+                    break;
+            }
+            Console.WriteLine(s);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
