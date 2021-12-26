184181fa69f528e0ba0db4684883af82f2689149
Yura Boytsun <sicksneg@gmail.com>
2021-12-26 22:20:39 +0100

    update task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,15 +5,34 @@ namespace HW3._2
     
     class Program
     {
-        
+       
+       static public void MyForm()
+        {
+            int num;
+            int result=0;
+            string nums= "Складываемые числа:";
+            do
+            {
+                Console.WriteLine("Введите число. Для выхода введите 0");
+                 int.TryParse(Console.ReadLine(),out num);
+                if (num % 2 == 0 && num!=0) { result += num; nums = nums + " " + num; }
+            }
+            while (num != 0);
+            Console.WriteLine($"{nums}. Результат: {result}");
+        }
       
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число. Чтобы завершить введите 0.");
-            Nums input = new Nums();
-            input.inputNum = int.Parse(Console.ReadLine());
-            Console.Write($"Сумма всех нечётных чисел введённых вами состовляет {Nums.MyNums()}");
-            Console.ReadLine();
+            Console.WriteLine(@"
+С клавиатуры вводятся числа, пока не будет введён 0 
+(каждое число в новой строке). Требуется подсчитать сумму всех нечётных положительных чисел.
+Сами числа и сумму вывести на экран, используя tryParse.
+
+");
+            MyForm();
+            Console.ReadKey();
+
+            
         }
     }
 }
